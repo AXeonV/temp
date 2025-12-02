bd338ae78fb28ebba7d6451567ffa9381629d82e
AXeonV <2607343351@qq.com>
2025-12-02 21:59:16 +0800

    update on 2 Dec

--- test.cpp
+++ test.cpp
@@ -1,10 +1,38 @@
-#include <iostream>
-#include "test.h"
+#include <bits/stdc++.h>
 using namespace std;
-
+const int MAXN = 1e3 + 6;
+int a[MAXN];
 int main() {
-	int a = 123, b = 456;
-	swap_2i(a, b);
-	cout << a << " " << b << endl;
+	freopen("data.in", "r", stdin);
+	freopen("data.out", "w", stdout);
+	int n; scanf("%d\n", &n);
+	while (n) {
+		int tot = 0;
+		memset(a, 0, sizeof a);
+		while (n) {
+			a[++tot] = n & 1;
+			n >>= 1;
+		}
+		int ans = tot - 1, cnt = 0;
+		for (int i = 1; i <= tot; ++i)
+			cout << a[i] << " ";
+		cout << endl;
+		for (int i = 1; i <= tot + 1; ++i) {
+			if (a[i]) ++cnt;
+			else {
+				if (!cnt) continue;
+				if (i == tot + 1) {
+					if (cnt > 2) ans += 2;
+					else ++ans;
+				} else {
+					if (cnt > 1) ans += 2;
+					else ++ans;
+				}
+				cnt = 0;
+			}
+		}
+		printf("%d\n", ans);
+		scanf("%d", &n);
+	}
 	return 0;
 }
--- test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void swap_2i(int &a, int &b) {
-	int tmp = a;
-	a = b;
-	b = tmp;
-}
